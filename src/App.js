d7f034a69881292e0cfb4f6d70379af74c6b5856
AkshatSrivastava2 <akshatsrivastava2@gmail.com>
2017-02-24 01:17:46 +0530

    Ball render and bar code added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 import Ball from './Ball.js';
 
 class App extends Component
@@ -18,26 +18,43 @@ class App extends Component
     }
     this.ball=[];
   }
+handleResize(value,e)
+  {
+    this.setState({
+      screen: {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        ratio: window.devicePixelRatio || 1,
+      }
+    });
+  }
 
   componentDidMount() 
   {
     const context = this.refs.canvas.getContext('2d');
     this.setState({ context: context });
-    context.fillRect(0,0,this.state.screen.width, this.state.screen.height);
+    context.arc(this.state.screen.width/2,this.state.screen.height/2,50,0,2*Math.PI,true);
     requestAnimationFrame(() => {this.update()});
 
+  }
+  componentWillUnmount()
+  {
+    window.removeEventListener('resize', this.handleResize);
   }
   update()
   {
     const context = this.state.context;
+    const ball=this.ball[0];
     context.save();
-    context.scale(this.state.screen.ratio, this.state.screen.height);
+    context.scale(this.state.screen.ratio, this.state.screen.ratio);
 
-    context.fillStyle ='white';
-    context.globalAlpha = 0.4;
-    context.fillRect(0,0,this.state.screen.width, this.state.screen.height);
-    context.globalAlpha = 1;
-
+    context.fillStyle ='red';
+    //context.globalAlpha = 0.4;
+    //context.fillRect(0,0,this.state.screen.width, this.state.screen.height);
+    context.arc(0, 0, 2, 0, 2 * Math.PI,true);
+    context.closePath();
+    context.fill();
+    //context.globalAlpha = 1;
     context.restore();
     requestAnimationFrame(() => {this.update()});
 
@@ -45,24 +62,38 @@ class App extends Component
   startGame(){
   let  ball= new Ball({
       position: {
-        x: this.state.screen.width/2,
-        y: this.state.screen.height/2
+        x: this.state.screen.width/2-200,
+        y: this.state.screen.height/2-200
       },
       create: this.createObject.bind(this),
       //onDie: this.gameOver.bind(this)
     });
     this.createObject(ball, 'ball');
 }
-  render()
+addObject(item,group) 
   {
-    return(
-      <div>
+    this[group].push(item);
+  }
+
+
+  updateObjects(items,group)
+  {
+    let index=0;
+    for(let item of items)
+    {
+      items[index].render(this.state);
+      index++;
+    }
+  }
+    render()
+  {
+    return(<div>
       <canvas ref="canvas"
-      width={this.state.screen.width*this.state.screen.ratio}
-      height={this.state.screen.height*this.state.screen.ratio}
+      width={this.state.screen.width * this.state.screen.ratio}
+      height={this.state.screen.height * this.state.screen.ratio}
       />
       </div>
-      );
+    );
   }
 }
 export default App;
--- src/Ball.js
+++ src/Ball.js
@@ -1,27 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 //import './App.css';
-class Ball{
-	constructor(args) {
-    this.position = args.position
-    this.radius=args.size;
-    this.x=window.innerWidth/2;
-    this.y=window.innerHeight/2;
-    this.rotation=0;
-}
-render(state){
-	// Draw
-     const context = state.context;
-    context.save();
-    context.translate(this.position.x, this.position.y);
-    context.rotate(this.rotation * Math.PI / 180);
-    context.fillStyle = '#FFF';
-    context.lineWidth = 0,5;
-    context.beginPath();
-    context.arc(0, 0, 2, 0, 2 * Math.PI);
-    context.closePath();
-    context.fill();
-    context.restore();
-  }
+class Ball
+{
+	constructor()
+	{
+		this.position={
+			x:window.innerWidth/2-300,
+			y:window.innerHeight-40
+		},
+		this.size={
+			radius:25
+		}
+	}
+	render(state){
+		const context=state.context;
+		context.save();
+		context.beginPath();
+		context.fillStyle='red';
+		context.arc(0,0,20,0,2*Math.PI,true);
+		context.fill();
+		context.closePath();
+	}
 }
 export default Ball;
