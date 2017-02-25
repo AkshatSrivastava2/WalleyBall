b18b615853e15e3c1ad088a3ced5165f541b0134
Saurabh0707 <imtemptedguy@gmail.com>
2017-02-25 18:17:19 +0530

    directory structure

---
+++ src/App.js
@@ -0,0 +1,179 @@
+import React, { Component } from 'react';
+import Ball from './Ball.js';
+import Bars from './Bars.js';
+
+class App extends Component
+{
+  constructor()
+  {
+    super();
+    this.state = {
+      screen: {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        ratio: window.DevicePixelRatio || 1,
+      },
+      context: null,
+    }
+    this.ball=[];
+    this.bars=[];
+    inGame: false
+  }
+handleResize(value,e)
+  {
+    this.setState({
+      screen: {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        ratio: window.devicePixelRatio || 1,
+      }
+    });
+  }
+
+  componentDidMount() 
+  {
+    window.addEventListener('resize', this.handleResize.bind(this, false));
+    const context = this.refs.canvas.getContext('2d');
+    this.setState({ context: context });
+    let xcoor= window.innerWidth/2;
+    let ycoor= window.innerHeight/2;
+    let radius= 25;
+    let color= 'red';
+    let velocityx=0;
+    let velocityy=0; 
+    this.makeBall(xcoor,ycoor,radius,color, velocityx, velocityy);
+    this.makeBars();
+    requestAnimationFrame(() => {this.update()});
+
+  }
+  componentWillUnmount()
+  {
+    window.removeEventListener('resize', this.handleResize);
+  }
+  update()
+  {
+    const context = this.state.context;
+    const ball=this.ball[0];
+    const bars=this.bars[4];
+    context.save();
+    context.scale(this.state.screen.ratio, this.state.screen.ratio);
+
+    context.fillStyle ='#000';
+    context.fillRect(0,0,this.state.screen.width, this.state.screen.height);
+    this.updateObjects(this.ball, 'ball');
+    this.updateObjects(this.bars, 'bars');
+    context.restore();
+    requestAnimationFrame(()=>{this.update()});
+  }
+
+makeBall(x,y,r,c, vx, vy)
+{
+  let  ball= new Ball(
+  	{
+  		position:
+  		{
+			xcoor:x,
+			ycoor:y
+		},
+		velocity : {
+			x: vx,
+			y: vy
+		},
+		size:
+		{
+			radius:r,
+			color:c
+
+		}
+  	});
+    this.ball=[];
+    this.addObject(ball, 'ball');
+}
+
+makeBars()
+  {
+    let xCoordinate = [(this.state.screen.width/2)-200,(this.state.screen.width/2)-200,5,window.innerWidth-35];
+    let yCoordinate = [5,window.innerHeight-25, window.innerHeight/2-100, window.innerHeight/2-100];
+    let barLength = [400,400,20,20];
+    let barWidth = [20,20,200,200];
+    let colors = ['red','blue','green','yellow'];
+    for(let i=0;i<4;i++)
+    {
+      let bars = new Bars({
+        position: {
+          x: xCoordinate[i],
+          y: yCoordinate[i]
+        },
+        size: {
+          length: barLength[i],
+          width: barWidth[i]
+        },
+        color: {
+          barColor: colors[i],
+        },
+      
+      });
+      this.addObject(bars, 'bars');
+    }
+  }
+
+  startGame(){
+    this.setState({
+      
+      inGame: true,
+     
+    });
+    let xCoordinate= (this.state.screen.width/2);
+    let yCoordinate= 50;
+    let radius= 25;
+    let color= 'red';
+    let velocityx=1;
+    let velocityy=1; 
+    this.makeBall(xCoordinate,yCoordinate,radius,color, velocityx, velocityy);
+    requestAnimationFrame(() => {this.update()});
+  }
+
+addObject(item,group) 
+  {
+    this[group].push(item);
+  }
+
+
+  updateObjects(items,group)
+  {
+    let index=0;
+    for(let item of items)
+    {
+      items[index].render(this.state);
+      index++;
+    }
+  }
+
+
+  
+    render()
+  {
+    let startgamebutton;
+  	 if(!this.state.inGame){
+        startgamebutton = (
+        <div>
+        <button
+            onClick={ this.startGame.bind(this) }>
+            Start Game
+        </button>
+        </div>
+      )
+    }
+
+    return(<div>
+      
+      {startgamebutton} 
+      <canvas ref="canvas"
+      width={this.state.screen.width * this.state.screen.ratio}
+      height={this.state.screen.height * this.state.screen.ratio}
+      />
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/Ball.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+//import './App.css';
+class Ball
+{
+
+	constructor(args)
+	{
+		this.position =
+		{
+			x:args.position.xcoor,
+			y:args.position.ycoor,
+			
+		},
+		this.velocity = {
+			x: args.velocity.x,
+			y: args.velocity.y
+		},
+  
+		this.size =
+		{ 
+			radius: args.size.radius,
+		    color : args.size.color
+	    }
+	}
+	render(state){
+		let color = ['red','green','blue','yellow'];
+			//right bar
+			if(this.position.x > state.screen.width + this.size.radius-75)
+			{ 
+				this.velocity.x = -this.velocity.x;
+				this.size.color = color[Math.floor(Math.random() * 3) + 1];
+			}
+
+    	//left bar
+    	else if(this.position.x < -this.size.radius+70) 
+    	{
+    		this.velocity.x = (state.screen.width + this.velocity.x);
+    		this.size.color = color[Math.floor(Math.random() * 3) + 1];
+
+    	}
+
+       	//bottom bar
+       	if(this.position.y > state.screen.height + this.size.radius-75)
+       	{ 
+       		this.velocity.y = -this.velocity.y;
+       		this.size.color = color[Math.floor(Math.random() * 3) + 1];
+       	}
+
+    	//top bar
+    	else if(this.position.y < -this.size.radius+75) 
+    	{
+    		this.velocity.y = (state.screen.height + this.velocity.y);
+    		this.size.color=color[Math.floor(Math.random() * 3) + 1];
+    	}
+
+    	this.position.x += this.velocity.x;
+    	this.position.y += this.velocity.y;	
+    	const context=state.context;
+    	context.save();
+    	context.beginPath();
+    	context.fillStyle=this.size.color;
+    	context.arc(this.position.x,this.position.y,this.size.radius,0,2*Math.PI,true);
+		//context.arc(this.position.x2,this.position.y2,this.size.radius,0,2*Math.PI,true);
+		context.fill();
+		context.closePath();
+	}
+}
+export default Ball;
